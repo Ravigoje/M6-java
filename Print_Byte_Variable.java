14ac3c42c7897daecbd16d9d393f184bfaa8b870
Ravigoje <115939474+Ravigoje@users.noreply.github.com>
2024-02-07 15:28:56 +0530

    Add files via upload

---
+++ Print_Byte_Variable.java
@@ -0,0 +1,15 @@
+//declaration and initialization of byte variable
+
+class Print_Byte_Variable
+{
+
+
+public static void main(String[]pbv)
+{
+
+byte variable1 = 20;
+System.out.println(variable1);
+}
+
+
+}
